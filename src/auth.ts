36e59d4a4d91004747012edffb1c144c82aca675
Rekhet <anubis@snu.ac.kr>
2026-01-18 19:51:10 -0800

    fix: revert manual auth config and upgrade auth libraries

--- src/auth.ts
+++ src/auth.ts
@@ -4,13 +4,6 @@ import { env } from '$env/dynamic/private';
 
 const ALLOWED_DOMAIN = 'snu.ac.kr';
 
-// Fix for Vercel: explicit AUTH_URL ensures basePath resolution works
-// Prioritize process.env.VERCEL_URL if AUTH_URL is missing
-if (!env.AUTH_URL && process.env.VERCEL_URL) {
-    // VERCEL_URL doesn't include https://
-    process.env.AUTH_URL = `https://${process.env.VERCEL_URL}/auth`;
-}
-
 export const { handle, signIn, signOut } = SvelteKitAuth({
 	providers: [
 		Google({
@@ -20,7 +13,6 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
 	],
 	secret: env.AUTH_SECRET,
 	trustHost: true,
-    basePath: '/auth',
 	callbacks: {
 		signIn: async ({ user }) => {
 			const email = user.email;
