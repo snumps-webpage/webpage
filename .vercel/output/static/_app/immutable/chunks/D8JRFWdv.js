42c8ffd53d40286a6347fd316a29c8974a8c52e1
Rekhet <anubis@snu.ac.kr>
2026-02-09 03:47:20 -0800

    chore: remove .vercel build artifacts from tracking

--- .vercel/output/static/_app/immutable/chunks/D8JRFWdv.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1nknaii?.base??"",a=globalThis.__sveltekit_1nknaii?.assets??s??"";export{a,s as b};
--- .vercel/output/static/_app/immutable/nodes/2.DOVBXCWQ.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/DsnmJJEf.js";import{c as n,f as m,a as p}from"../chunks/DgK-BC5G.js";import{s as e}from"../chunks/DmlEswr8.js";function f(a,r){var o=n(),t=m(o);e(t,()=>r.children),p(a,o)}export{f as component};
